5dc72a8438daedcea670a63fc24772d90cf77786
Venatik <stefan.trajkovski008@gmail.com>
2024-05-14 20:57:09 +0200

    db

--- Class 01/edit_data.sql
+++ Class 01/edit_data.sql
@@ -1,5 +1,18 @@
 SELECT * FROM achievement_type
+SELECT * FROM student
+SELECT * FROM teacher
 
 UPDATE achievement_type
 SET participation_rate = 75
 WHERE id = 2
+
+ALTER TABLE grade_details
+ADD CONSTRAINT fk_achievement_type
+FOREIGN KEY (achievement_type_id) REFERENCES achievement_type(id) 
+
+UPDATE student SET first_name = 'James' WHERE id = 1;
+
+UPDATE teacher SET academic_rank = 'Sr. Professor' WHERE id = 1;
+
+INSERT INTO achievement_type (name, description, participation_rate)
+VALUES ('Project', 'Final Project', 100);
